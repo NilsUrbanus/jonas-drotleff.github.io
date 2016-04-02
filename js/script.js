7fed71159029b3988941c43917e6676319c3f0d0
Jonas Drotleff <jonas.drotleff@gmail.com>
2016-04-02 13:48:09 +0200

    updated layout
    removed title (upper left corner)
    added h1 animation on home
    added parallax effect for home
    added home container for menu

--- js/script.js
+++ js/script.js
@@ -1,44 +1,54 @@
 $(function() {
-  var $trigger = $('.shadow-trigger');
-  var $headerShadow = $('.shadow');
-  $headerShadow.css({
-    'opacity': '1.0'
-  });
-  $headerShadow.hide();
-  var waypoints = $trigger.waypoint({
-    handler: function(direction) {
-      if (direction == 'down') {
-        $headerShadow.fadeIn(200);
-      } else if (direction == 'up') {
-        $headerShadow.fadeOut(200);
-      }
-    }
-  })
+    var $trigger = $('.shadow-trigger');
+    var $headerShadow = $('.shadow');
+    $headerShadow.css({
+        'opacity': '1.0'
+    });
+    $headerShadow.hide();
+    var waypoints = $trigger.waypoint({
+        handler: function(direction) {
+            if (direction == 'down') {
+                $headerShadow.fadeIn(200);
+            } else if (direction == 'up') {
+                $headerShadow.fadeOut(200);
+            }
+        }
+    })
 });
 $(window).scroll(function(event) {
-  var scroll = $(window).scrollTop();
-  scroll = .5 * (scroll / $(window).height());
-  console.log(scroll);
-  if (scroll <= 0.5) {
-    console.log('didit');
-    $('.fadeBackground').css({
-      'background-color': 'rgba(0,0,0,' + scroll + ')'
-    });
-  } else {
-    $('.fadeBackground').css({
-      'background-color': 'rgba(0,0,0,0.5)'
-    });
-  }
+    var scroll = $(window).scrollTop();
+    scroll = .3 * (scroll / $(window).height());
+    if (scroll <= 0.3) {
+        $('.fadeBackground').css({
+            'background-color': 'rgba(0,0,0,' + scroll + ')'
+        });
+        $('.home-container').css({
+            'top': -98 * (scroll / .3) + 'px'
+        });
+        $('.home-container h1').css({
+            'margin-top': 24 * (scroll / .3) + 'px'
+        })
+    } else {
+        $('.fadeBackground').css({
+            'background-color': 'rgba(0,0,0,0.3)'
+        });
+    }
+});
+
+$(document).ready(function() {
+    $('.home-container h1').css({
+        'animation': 'fade-in-h1 1.25s cubic-bezier(0.35, 0.36, 0, 1.01) .5s forwards'
+    })
 });
 
 $(function() {
-  var toggle = $('#drawer-toggle');
-  var link = $('.header__nav')
-  var overlay = $('.drawer-content-overlay');
-  overlay.on('click', function(event) {
-    toggle.prop('checked', false);
-  });
-  link.on('click', function(event) {
-    toggle.prop('checked', false);
-  });
+    var toggle = $('#drawer-toggle');
+    var link = $('.header__nav')
+    var overlay = $('.drawer-content-overlay');
+    overlay.on('click', function(event) {
+        toggle.prop('checked', false);
+    });
+    link.on('click', function(event) {
+        toggle.prop('checked', false);
+    });
 });
