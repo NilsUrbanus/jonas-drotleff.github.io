88490f941d65d8f19774b44c850f5827b4287756
Jonas Drotleff <jonas.drotleff@gmail.com>
2016-05-23 18:47:38 +0200

    placeholder

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    $("#typed").typed({
+        cursorChar: "",
+        typeSpeed: 50,
+        stringsElement: $('#typed-strings'),
+    });
+    setTimeout(function() {
+        $('.comingsoon').fadeOut(500, function() {});
+    }, 6000);
+});
